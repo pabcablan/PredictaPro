64d1ec062f18cbc71fbf186b728e8f1afff1a6c8
Pablo <pabcablan2005@gmail.com>
2024-12-16 22:22:17 +0000

    PredictionResult Attributes and Constructor

--- src/es/predictapro/model/PredictionResult.java
+++ src/es/predictapro/model/PredictionResult.java
@@ -1,4 +1,33 @@
 package es.predictapro.model;
 
+
+/**
+ * PredictionResult represents the outcome of a prediction process.
+ * <p>
+ * It encapsulates:
+ * - The predicted value as a numerical result.
+ * - A message providing context or additional information about the prediction.
+ * </p>
+ */
 public class PredictionResult {
+
+
+    // The value predicted by the prediction process.
+    private double predictedValue;
+
+
+    // A message describing the prediction or its context.
+    private String message;
+
+
+    /**
+     * Constructor for PredictionResult.
+     *
+     * @param predictedValue The numerical value resulting from the prediction.
+     * @param message        A message providing context or details about the prediction result.
+     */
+    public PredictionResult(double predictedValue, String message) {
+        this.predictedValue = predictedValue;
+        this.message = message;
+    }
 }
