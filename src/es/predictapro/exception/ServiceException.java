6f4204a9604da08f125a52ffe86b18ab13ecf6eb
Pablo <pabcablan2005@gmail.com>
2024-12-16 22:36:54 +0000

    Branch Exception Done

---
+++ src/es/predictapro/exception/ServiceException.java
@@ -0,0 +1,7 @@
+package es.predictapro.exception;
+
+public class ServiceException extends RuntimeException {
+    public ServiceException(String message) {
+        super(message);
+    }
+}
---
+++ src/es/predictapro/exception/ValidationException.java
@@ -0,0 +1,7 @@
+package es.predictapro.exception;
+
+public class ValidationException extends RuntimeException {
+    public ValidationException(String message) {
+        super(message);
+    }
+}
